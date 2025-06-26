b9512ab9e6cdcebd9972e508544eeacf105c976a
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2025-06-26 23:07:40 +0530

    Successfully integrated nebular module for chat layout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { ProgressComponent } from '../progress/progress.component';
-import { NbChatComponent, NbChatModule, NbThemeModule } from '@nebular/theme';
+import { NbChatComponent, NbChatModule, NbLayoutModule, NbThemeModule } from '@nebular/theme';
 import { ChatShowcaseComponent } from '../chat-showcase/chat-showcase.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ChatShowcaseComponent],
+  imports: [ChatShowcaseComponent, NbLayoutModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
