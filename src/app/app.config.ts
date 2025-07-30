4c6a811e8c1ad763deca0b28276af95040dec1fb
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2025-07-30 23:53:27 +0530

    Working on sidebar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { NbThemeModule } from '@nebular/theme';
+import { NbSidebarModule, NbThemeModule, } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    importProvidersFrom(NbThemeModule.forRoot({ name: 'default' })),
+    importProvidersFrom(NbThemeModule.forRoot({ name: 'default' }), NbSidebarModule.forRoot(), NbEvaIconsModule),
     provideAnimations(),
     provideClientHydration(withEventReplay())
   ]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { ProgressComponent } from '../progress/progress.component';
-import { NbChatComponent, NbChatModule, NbLayoutModule, NbThemeModule } from '@nebular/theme';
+import { NbChatComponent, NbChatModule, NbIconComponent, NbIconModule, NbLayoutComponent, NbLayoutModule, NbSidebarComponent, NbSidebarModule, NbSidebarService, NbThemeModule } from '@nebular/theme';
 import { ChatShowcaseComponent } from '../chat-showcase/chat-showcase.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ChatShowcaseComponent, NbLayoutModule, ProgressComponent],
+  // imports: [ChatShowcaseComponent, NbLayoutModule, ProgressComponent],
+  imports: [NbLayoutModule, NbSidebarModule, ChatShowcaseComponent, NbIconModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
 
+export class DashboardComponent {
+  constructor(private sidebarService: NbSidebarService) {
+    // Initialization logic can go here if needed
+  }
+
+  toggleSidebar() {
+    this.sidebarService.toggle();
+  }
 }
