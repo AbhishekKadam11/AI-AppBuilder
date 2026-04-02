1e19769fc14e5eb8d8c5c9ac3dd195b3d2ab303a
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2026-04-02 23:41:11 +0530

    Working on broswer node for swimlane

--- src/app/swimlane/swimlane-dashboard/swimlane-dashboard.component.ts
+++ src/app/swimlane/swimlane-dashboard/swimlane-dashboard.component.ts
@@ -84,12 +84,15 @@ export class SwimlaneDashboardComponent {
   private directorySubscription: Subscription | undefined
   dataSource: any
   appDiagramSchema: any = {};
+  private isWebContainerActive: boolean = false;
+  collectionLogs: string[] = [];
+  samelineLogs!: string;
 
   constructor(private socketService: SocketService,
     private webContainerService: WebContainerService,
     private appWorkflowService: AppWorkflowService,
   ) {
-    console.log("appObject in swimlane constructor:", this.appWorkflowService.appObject$);
+    // console.log("appObject in swimlane constructor:", this.appWorkflowService.appObject$);
   }
 
   ngAfterViewInit() {
@@ -106,16 +109,26 @@ export class SwimlaneDashboardComponent {
     //   });
     // }
     this.appWorkflowService.appObject$.subscribe((appDetails: any) => {
-      console.log('Received appDetails in SwimlaneDashboardComponent:', appDetails);
+      // console.log('Received appDetails in SwimlaneDashboardComponent:', appDetails);
       if (appDetails && appDetails.data.extraConfig.projectName) {
         this.messages = { "action": "getContainerFiles", "path": appDetails.data.extraConfig.projectName };
         this.socketService.sendMessage(this.directoryManager, this.messages);
         const serverReply$ = this.socketService?.on(this.directoryManager);
         if (serverReply$) {
           this.directorySubscription = serverReply$.subscribe((response: any) => {
-            console.log('Received directorySubscription from server:', response);
+            // console.log('Received directorySubscription from server:', response);
             const formatedTree: TreeNode<FSEntry>[] = this.webContainerService.transformToNebularTree(response.data);
-            console.log('formatedTree', formatedTree);
+            // console.log('formatedTree', formatedTree);
+            if (!this.isWebContainerActive) {
+              this.webContainerService.bootAndRun(response.data[appDetails.data.extraConfig.projectName]['directory']);
+              this.isWebContainerActive = true;
+              this.webContainerService.output$.subscribe(log => {
+                this.appendLog(log);
+              });
+            } else {
+              console.log('WebContainer is already active. Skipping boot.');
+              this.webContainerService.mountFiles(response.data[appDetails.data.extraConfig.projectName]['directory']);
+            }
             this.destructringAssignment(formatedTree);
             this.nodeGeneration(); // Generate nodes after processing the file tree and routes
           });
@@ -181,7 +194,7 @@ export class SwimlaneDashboardComponent {
   nodeGeneration() {
     //iterate over enum NodeTypes and generate nodes for each type
     for (const nodeType in NodeTypes) {
-      console.log('Generating nodes for type:', NodeTypes[nodeType as keyof typeof NodeTypes]);
+      // console.log('Generating nodes for type:', NodeTypes[nodeType as keyof typeof NodeTypes]);
       const dataSource = this.appDiagramSchema[NodeTypes[nodeType as keyof typeof NodeTypes]]?.dataSource || [];
       //break component tre to sperate nodes for each component
       if (NodeTypes[nodeType as keyof typeof NodeTypes] === NodeTypes.ComponentTree) {
@@ -207,7 +220,7 @@ export class SwimlaneDashboardComponent {
             id: `id-${nodeType}-${route.path}`,
             type: NodeTypes[nodeType as keyof typeof NodeTypes],
             position: { x: 100 + Object.keys(NodeTypes).indexOf(nodeType) * 400, y: 100 + routeDataSource.indexOf(route) * 570 },
-            size: { width: 860, height: 1520 },
+            size: { width: 860, height: 860 },
             autoSize: false,
             resizable: true,  // Enable resizing for the node
             data: { appName: 'loginApp5', label: NodeLabels[nodeType as keyof typeof NodeLabels], type: 'rootNode', dataSource: [route], attribute: { icon: 'angular-logo', url: '' } }
@@ -227,9 +240,24 @@ export class SwimlaneDashboardComponent {
       });
     }
 
-    console.log('Generated nodes:', this.nodes);
+    // console.log('Generated nodes:', this.nodes);
     this.model = initializeModel({
       nodes: this.nodes
     }, this.injector);
   }
+  private appendLog(log: string): void {
+    const samelineWords = ['/', '-', '\\', '|', '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
+    const timestamp = `[${new Date().toISOString()}]`;
+    if (samelineWords.includes(log.trim().charAt(0))) {
+      // Handle carriage return: overwrite the last line
+      // console.log('sameline log:', log);
+      this.samelineLogs = log != undefined ? `[client] ${timestamp}: ${log.trim()}` : '';
+    } else {
+      if (log != undefined && log.trim() != '') {
+        this.collectionLogs.push(`[client] ${timestamp}: ${log}`);
+      }
+    }
+    console.log("this.collectionLogs in swimlane:", this.collectionLogs);
+  }
+
 }
