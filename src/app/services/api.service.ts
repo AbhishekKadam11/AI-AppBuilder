86e5798ceaaf7d69f84c1eb547fb794ada412b0f
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2026-07-31 22:51:15 +0530

    working on auth interceptor issue on get api request

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,18 +1,29 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap, catchError, of } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { error } from 'console';
 
 @Injectable({
   providedIn: 'root' // Ensures a single, application-wide instance (singleton)
 })
 export class ApiService {
-  private apiUrl = environment.apiUrl;
-
-  constructor(private http: HttpClient) {}
+  private readonly apiUrl = environment.apiUrl;
+  private readonly http = inject(HttpClient);
+  constructor() { }
 
   get<T>(path: string): Observable<T> {
-    return this.http.get<T>(`${this.apiUrl}/${path}`, { responseType: 'json' });
+    console.log("api url", this.apiUrl);
+    return this.http.get<T>(`${this.apiUrl}/${path}`, {
+      withCredentials: true,
+      responseType: 'json'
+    }).pipe(
+      tap((response) => {
+        console.log("testing", response);
+      }, (error) => {
+        console.log("testing ", error);
+      })
+    );
   }
 
   post<T>(path: string, body: any): Observable<T> {
